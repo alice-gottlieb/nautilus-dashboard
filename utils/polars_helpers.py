1607645bd37df58d7147922267212338c302c44b
alice-gottlieb <aliceagottlieb@gmail.com>
2023-10-18 15:26:34 -0700

    Format polars_helper.py with Black

--- utils/polars_helpers.py
+++ utils/polars_helpers.py
@@ -4,6 +4,7 @@ Collection of helper functions for lazy-loading and collating CSV data
 import polars as pl
 import urllib.request
 
+
 def default_csv_downloader(url, savepath):
     """
     :brief: placeholder function that just calls
@@ -14,7 +15,8 @@ def default_csv_downloader(url, savepath):
     :param savepath: filepath to save the CSV to
     :return: Filename under which the path can be found.
     """
-    return urllib.request.urlretrieve(url,filename=savepath)[0]
+    return urllib.request.urlretrieve(url, filename=savepath)[0]
+
 
 def hist_expr_builder(column_name: str, ranges: list) -> pl.Expr:
     """
@@ -24,11 +26,14 @@ def hist_expr_builder(column_name: str, ranges: list) -> pl.Expr:
     :param column_name: Name of column to look at for values.
     :param
     """
-    range_expr = pl.when(pl.col(column_name) != pl.col(column_name)).then([0.0,0.0])
+    range_expr = pl.when(pl.col(column_name) != pl.col(column_name)).then([0.0, 0.0])
     for start, end in ranges:
-        range_expr = range_expr.when(pl.col(column_name).is_between(pl.lit(start),pl.lit(end),'left')).then([start,end])
-    range_expr = range_expr.otherwise([0.0,0.0]).alias("hist_bin")
+        range_expr = range_expr.when(
+            pl.col(column_name).is_between(pl.lit(start), pl.lit(end), "left")
+        ).then([start, end])
+    range_expr = range_expr.otherwise([0.0, 0.0]).alias("hist_bin")
     return range_expr
+
 
 def get_histogram_from_file(filepath, column_name, ranges):
     """
@@ -41,5 +46,10 @@ def get_histogram_from_file(filepath, column_name, ranges):
         with two columns, "hist_bin" (histogram ranges, each in the form of a two-item list),
         and "count" (the number of rows where the indicated column was in that range)
     """
-    q = (pl.scan_csv(filepath).select(pl.col(column_name),hist_expr_builder(column_name,ranges)).group_by("hist_bin").count())
+    q = (
+        pl.scan_csv(filepath)
+        .select(pl.col(column_name), hist_expr_builder(column_name, ranges))
+        .group_by("hist_bin")
+        .count()
+    )
     return q
