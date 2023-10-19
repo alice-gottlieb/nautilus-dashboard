2dc4339a584ec58fc4ca2fcd707871ad3d75efc4
Ali Cataltepe <ali@cataltepe.com>
2023-10-19 14:13:58 -0700

    wrote stubs for database accessors

--- utils/demo_io.py
+++ utils/demo_io.py
@@ -1,2 +1,3 @@
 import polars as pl
 import glob
+
--- utils/polars_helpers.py
+++ utils/polars_helpers.py
@@ -1,8 +1,117 @@
 """
-Collection of helper functions for lazy-loading and collating CSV data
+Collection of helper functions for polars operations, including FOV/spot selection.
 """
 import polars as pl
 import urllib.request
+
+
+def get_spots_from_fovs(fov_frame, fov_ids, rel_method, **kwargs):  # -> pl.Expr:
+    """
+    :brief: get a lazy-evaluable expression corresponding to the rows from
+        the spot table corresponding to the listed slides
+    :param fov_frame: dataframe-like (either expression or actual DF)
+        containing per-FOV info. Column structure of this data:
+        ====
+        slide_label (string, unique ID for containing slide over multiple timestamps)
+        id_in_slide (int, unique ID for FOV within slide)
+        timestamp (datetime, time of slide timestep acquisition)
+        ====
+    :param fov_ids: list where each entry is a tuple in the form
+        (slide_label,timestamp_start, timestamp_end, list_of_ids), where the list_of_ids
+        is a list that
+        can be empty to select all fovs within that timestamp range on that slide
+    :param rel_method: function that takes a pl.Expr corresponding to the appropriate
+        FOVs as input and returns a pl.Expr corresponding to the appropriate rows (associated
+        to the selected FOVs) from the spot table as output, taking any additional kwargs
+        as input
+    :return: pl.Expr corresponding to the desired rows from the spot table, which has column
+        ====
+        structure as follows:
+        slide_label (string)
+        timestamp (datetime)
+        fov_id (int) (id within slide of associated fov)
+        spot_id (int)
+        score (float) (assigned probability that spot is a positive detection)
+        (optional) coords_in_slide (float 2-tuple) (mm, mm) (for display of spot
+            on deepzoom overview of slide)
+        ====
+    """
+    pass
+
+
+def default_relate_fovs_to_spots(rel_df, spot_df, fov_rows):  # ->pl.Expr:
+    """
+    :brief: given a pl.Expr corresponding to rows from the FOVs table,
+        get a pl.Expr corresponding to rows from the spots table
+    :param rel_df: Dataframe-like (expression or actual DF) corresponding
+        to the relation table from FOVs to spots, with column structure
+        as follows:
+        ====
+        slide_label (string, UID of slide over multiple timestamps)
+        timestamp (datetime)
+        fov_id (int) (id within slide of FOV)
+        spot_id (int)
+        (optional) coords_in_fov (int 2-tuple, pixels)
+        ====
+    :param spot_df: Dataframe-like corresponding to spot dataset, specification
+        given in get_spots_from_fovs
+    :param fov_rows: pl.Expr corresponding to appropriate rows from FOV dataset (structure
+        given in get_spots_from_fovs)
+    :return: pl.Expr corresponding to appropriate rows from spot_df
+    """
+    pass
+
+
+def get_fovs_from_slides(
+    slide_frame, slide_labels, rel_method, **kwargs
+):  # -> pl.Expr:
+    """
+    :brief: get a lazy-evaluable expression corresponding to the
+        rows from the FOV table corresponding to the listed slides
+    :param slide_frame: dataframe-like (either expression or actual DF)
+        containing per-slide-time-step info. Column structure of this data:
+        ====
+        label (string, unique ID for slide over multiple timestamps)
+        name (string, human-readable)
+        timestamp (datetime, for acquisition)
+        total_rbc (int, est. total rbc counted on slide at this acquisition)
+        ====
+    :param slide_labels: list where each entry is a tuple (label, timestamp_start,
+        timestamp_end), where either of timestamp_start/end can be None to open the
+        interval on that end
+    :param rel_method: function that takes a pl.Expr corresponding to
+        the appropriate slide label/timestamp rows as input and returns a pl.Expr
+        corresponding to the appropriate rows from the FOV table, taking any additional
+        kwargs as input
+    :return: pl.Expr corresponding to the desired rows from the FOV table
+    """
+    pass
+
+
+def default_relate_slides_to_fovs(rel_df, fov_df, slide_rows):  # ->pl.Expr:
+    """
+    :brief: given a pl.Expr corresponding to rows from the slides table,
+        get a pl.Expr corresponding to rows from the FOVs table
+    :param rel_df: Dataframe-like (expression or actual DF) corresponding
+        to the relation table from slides to FOVs, with column structure as
+        follows:
+        ====
+        slide_label (string, unique ID of slide across timestamps)
+        timestamp (datetime)
+        fov_id_in_slide (int)
+        indices_in_slide (int 3-tuple (x,y,z)) assumed from bottom left
+        center_coords (float 3-tuple (x,y,z) (mm,mm,um)) coordinates
+            of camera motor at time of acquisition
+        (optional) corner_coords (float 4-tuple (x_top_left, y_top_left,
+            x_bottom_right,y_bottom_right) (mm,mm,mm,mm))
+        ====
+    :param fov_df: Dataframe-like corresponding to the FOV data, with column
+        structure given in get_spots_from_fovs
+    :param slide_rows: pl.Expr corresponding to desired slide/timestamps in
+        slide dataset (structure given in get_fovs_from_slides)
+    :return: pl.Expr corresponding to the desired rows from fov_df
+    """
+    pass
 
 
 def default_csv_downloader(url, savepath):
@@ -24,7 +133,8 @@ def hist_expr_builder(column_name: str, ranges: list) -> pl.Expr:
         then(...)...otherwise(...).alias("hist_bin")
         expression for grouping rows into histogram bins
     :param column_name: Name of column to look at for values.
-    :param
+    :param ranges: list in the form [(start1,end1),(start2,end2),...)] of ranges, assumed
+        closed on left only
     """
     range_expr = pl.when(pl.col(column_name) != pl.col(column_name)).then([0.0, 0.0])
     for start, end in ranges:
